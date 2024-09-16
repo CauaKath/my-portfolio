ae6b1ac40e4df355fe789e8ff05b9c91a9fcf97d
CauaKath <cauakathdev@gmail.com>
2024-09-15 22:40:10 -0300

    feat: initial blog page with static content

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="post" :class="type">
+    <div class="post-image" :class="type"></div>
+
+    <div class="post-content" :class="type">
+      <div class="post-main">
+        <div class="post-texts">
+          <span class="post-date">Aug 13 • 5 min read</span>
+          <span class="post-title">Pilha X Fila</span>
+          <span class="post-description">Qual a diferença? Onde utilizar cada uma? Exemplos práticos</span>
+        </div>
+
+        <img src="@/assets/add.svg" alt="Source icon">
+      </div>
+
+      <div class="post-tags" :class="type">
+        <span>#tech</span>
+        <span>#data-structure</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BlogPost',
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+      validator: (value: string) => ['default', 'most-recent', 'other-recent'].includes(value),
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.post {
+  @apply
+    flex
+    bg-red-300
+    rounded-md
+    h-full
+    w-full;
+
+    &.most-recent {
+      @apply
+        flex-col;
+    }
+
+    &.default {
+      @apply
+        flex-col;
+    }
+
+  .post-image {
+    @apply
+      w-full
+      bg-[url('../assets/banner.jpg')]
+      bg-center
+      bg-cover;
+
+    &.most-recent {
+      @apply
+        h-[300px]
+        rounded-t-md;
+    }
+
+    &.default {
+      @apply
+        h-[140px]
+        rounded-md;
+    }
+
+    &.other-recent {
+      @apply
+        rounded-l-md;
+    }
+  }
+
+  .post-content {
+    @apply
+      w-full
+      h-full
+      flex
+      flex-col
+      bg-white
+      p-4;
+
+      &.most-recent, &.default {
+        @apply
+          rounded-b-md;
+      }
+
+      &.other-recent {
+        @apply
+          rounded-r-md;
+      }
+
+    .post-main {
+      @apply
+        w-full
+        h-full
+        flex
+        items-start;
+
+      .post-texts {
+        @apply
+          w-full
+          h-full
+          text-sm
+          flex
+          flex-col
+          gap-2;
+
+        .post-date {
+          @apply
+            bg-gradient-to-r from-register-from to-register-to
+            inline-block
+            text-transparent
+            bg-clip-text;
+        }
+
+        .post-title {
+          @apply
+            text-xl
+            text-primary-default
+            font-bold;
+        }
+
+        .post-description {
+          @apply
+            h-[80px]
+            text-gray_text;
+        }
+      }
+
+      img {
+        @apply
+          w-6
+          h-6;
+      }
+    }
+
+    .post-tags {
+      @apply
+        text-sm
+        text-primary-default
+        flex
+        gap-2;
+
+      >span {
+        @apply
+          border-2
+          border-primary-default
+          px-2
+          py-1
+          rounded-md;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/Blog.vue
@@ -0,0 +1,287 @@
+<template>
+  <div class="blog">
+    <div class="banner">
+      <div>
+        <h1>Welcome to my Blog</h1>
+      </div>
+    </div>
+
+    <div class="recent">
+      <div class="recent-header">
+        <span>RECENT POSTS</span>
+
+        <div class="search-box">
+          <div class="search-input">
+            <img src="@/assets/search-gray.svg" alt="Search icon">
+            <input
+              type="text"
+              placeholder="Search for posts"
+            />
+          </div>
+
+          <div class="add-button">
+            <img src="@/assets/add.svg" alt="Add icon">
+            <span>Add</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="posts">
+        <BlogPost type="most-recent" />
+        
+        <div class="second-and-third">
+          <BlogPost type="other-recent" />
+          <BlogPost type="other-recent" />
+        </div>
+      </div>
+    </div>
+
+    <hr class="divider">
+
+    <div class="all">
+      <div class="all-header">
+        <span>ALL POSTS</span>
+      </div>
+
+      <div class="posts">
+        <table>
+          <tr>
+            <td>
+              <BlogPost />
+            </td>
+            <td>
+              <BlogPost />
+            </td>
+            <td>
+              <BlogPost />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <BlogPost />
+            </td>
+            <td>
+              <BlogPost />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import BlogPost from '@/components/BlogPost.vue';
+
+export default {
+  name: 'Blog',
+  components: {
+    BlogPost,
+  },
+}
+</script>
+
+<style lang="scss">
+.blog {
+  @apply
+    flex
+    items-center
+    flex-col
+    gap-16
+    py-16
+    h-full
+    bg-background
+    text-2xl;
+
+  .banner {
+    @apply
+      w-[80%]
+      py-16
+      flex
+      justify-center
+      items-center
+      bg-[url('../assets/banner.jpg')]
+      bg-center
+      bg-cover;
+    
+    div {
+      @apply
+        bg-[#F4F4F5]
+        bg-opacity-85
+        text-center
+        px-16
+        py-8;
+      
+      h1 {
+        @apply
+          text-4xl
+          font-bold
+          text-primary-default;
+      }
+    }
+  }
+
+  .recent {
+    @apply
+      w-[80%]
+      flex
+      justify-center
+      items-center
+      gap-16
+      flex-col;
+
+    .recent-header {
+      @apply
+        w-full
+        flex
+        justify-between
+        items-center;
+
+      >span {
+        @apply
+          text-2xl
+          font-bold
+          text-primary-default;
+      }
+
+      .search-box {
+        @apply
+          flex
+          justify-between
+          items-center
+          gap-6
+          w-[60%];
+
+        .search-input {
+          @apply
+            w-full
+            flex
+            justify-start
+            items-center
+            gap-2
+            px-3
+            py-2
+            text-sm
+            bg-white
+            text-gray_text
+            rounded-full;
+
+          >input {
+            @apply
+              w-full
+              bg-transparent
+              border-none
+              outline-none;
+          }
+        }
+
+        .add-button {
+          @apply
+            w-24
+            flex
+            justify-start
+            items-center
+            gap-3
+            px-3
+            py-2
+            text-sm
+            bg-primary-default
+            text-white
+            rounded-full
+            cursor-pointer;
+        }
+      }
+    }
+
+    .posts {
+      @apply
+        w-full
+        h-full
+        flex
+        gap-8
+        items-start;
+
+      .second-and-third {
+        @apply
+          w-full
+          flex
+          flex-col
+          self-stretch
+          justify-between
+          items-center
+          gap-8;
+      }
+    }
+  }
+
+  .divider {
+    @apply
+      w-[80%]
+      border-solid
+      border-light_border;
+  }
+
+  .all {
+    @apply
+      w-[80%]
+      flex
+      justify-center
+      items-center
+      gap-16
+      flex-col;
+
+    .all-header {
+      @apply
+        w-full
+        flex
+        justify-start
+        items-center
+        gap-4;
+
+      >span {
+        @apply
+          text-2xl
+          font-bold
+          text-primary-default;
+      }
+    }
+
+    .posts {
+      @apply
+        w-full
+        h-full
+        flex
+        flex-wrap
+        gap-8
+        items-start;
+
+      table {
+        @apply
+          border-spacing-2
+          w-full;
+
+        tr + tr {
+          @apply
+            mt-8;
+        }
+
+        tr {
+          @apply
+            w-full;
+
+          td {
+            @apply
+              w-[calc(100%/3)];
+          }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 780px) {
+  .blog {
+    @apply h-[calc(100vh-60px-40px)];
+  }
+}
+</style>
